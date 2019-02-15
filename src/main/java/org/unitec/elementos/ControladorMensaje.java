b3873e357700fa32fa1aab9278a8791d8eb536b1
campitos <rapidclimate@gmail.com>
2019-02-15 14:23:11 -0600

    clase 7 nde febrero

---
+++ src/main/java/org/unitec/elementos/ControladorMensaje.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ControladorMensaje {
+    
+    
+ @CrossOrigin   
+ @GetMapping("/mensaje")   
+ public String holaMundo(){
+     Mensaje mensa=new Mensaje();
+     mensa.setTitulo("Hola mundo");
+     return "Mi primer servicio REST accedido a origenes crzados";
+ }
+    
+}
